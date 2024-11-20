16c1f379a5ac6d5d3e1bafba3177b7e31698e1a9
Malcolm Hall <malhal@users.noreply.github.com>
2024-11-20 21:28:00 +0000

    Changed to Observable

--- SwiftUICoreDataFetchRequestRedesign/SwiftUICoreDataFetchRequestRedesign/FetchRequest2.swift
+++ SwiftUICoreDataFetchRequestRedesign/SwiftUICoreDataFetchRequestRedesign/FetchRequest2.swift
@@ -38,31 +38,31 @@ import Combine
     
     var sortDescriptors: [SortDescriptor<ResultType>] {
         get {
-            coordinator.fetchedResultsController?.fetchRequest.sortDescriptors?.compactMap { SortDescriptor($0, comparing: ResultType.self) } ?? []
+            coordinator.fetchRequest.sortDescriptors?.compactMap { SortDescriptor($0, comparing: ResultType.self) } ?? []
         }
         nonmutating set {
-            coordinator.fetchedResultsController?.fetchRequest.sortDescriptors = newValue.map { NSSortDescriptor($0) }
-            coordinator.result = nil
+            coordinator.fetchRequest.sortDescriptors = newValue.map { NSSortDescriptor($0) }
+            coordinator.fetchedResultsController = nil
         }
     }
     
     var nsSortDescriptors: [NSSortDescriptor] {
         get {
-            coordinator.fetchedResultsController?.fetchRequest.sortDescriptors ?? []
+            coordinator.fetchRequest.sortDescriptors ?? []
         }
         nonmutating set {
-            coordinator.fetchedResultsController?.fetchRequest.sortDescriptors = newValue
-            coordinator.result = nil
+            coordinator.fetchRequest.sortDescriptors = newValue
+            coordinator.fetchedResultsController = nil
         }
     }
     
     var nsPredicate: NSPredicate? {
         get {
-            coordinator.fetchedResultsController?.fetchRequest.predicate
+            coordinator.fetchRequest.predicate
         }
         nonmutating set {
-            coordinator.fetchedResultsController?.fetchRequest.predicate = newValue
-            coordinator.result = nil
+            coordinator.fetchRequest.predicate = newValue
+            coordinator.fetchedResultsController = nil
         }
     }
     
@@ -70,54 +70,82 @@ import Combine
         coordinator.result
     }
     
+    private var initialFetchRequest: NSFetchRequest<ResultType> {
+        let fr = NSFetchRequest<ResultType>(entityName: "\(ResultType.self)")
+        fr.predicate = initialNSPredicate()
+        if let initialNSSortDescriptors {
+            fr.sortDescriptors = initialNSSortDescriptors()
+        }
+        else {
+            fr.sortDescriptors = initialSortDescriptors.map { NSSortDescriptor($0) }
+        }
+        return fr
+    }
+    
     func update() {
-        if coordinator.fetchedResultsController?.managedObjectContext != viewContext {
-            let fr = coordinator.fetchedResultsController?.fetchRequest ?? {
-                let fr = NSFetchRequest<ResultType>(entityName: "\(ResultType.self)")
-                if let initialNSSortDescriptors {
-                    fr.sortDescriptors = initialNSSortDescriptors()
-                }
-                else {
-                    fr.sortDescriptors = initialSortDescriptors.map { NSSortDescriptor($0) }
-                }
-                fr.predicate = initialNSPredicate()
-                return fr
-            }()
-            coordinator.fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: viewContext, sectionNameKeyPath: nil, cacheName: nil)
+        if coordinator.managedObjectContext != viewContext {
+            coordinator.managedObjectContext = viewContext
+        }
+        if coordinator.fetchRequest == nil {
+            coordinator.fetchRequest = initialFetchRequest
         }
     }
     
+    @Observable
     class Coordinator: NSObject, NSFetchedResultsControllerDelegate, ObservableObject {
         
-        var fetchedResultsController: NSFetchedResultsController<ResultType>? {
+        @ObservationIgnored
+        var managedObjectContext: NSManagedObjectContext! {
+            didSet {
+                _fetchedResultsController = nil
+            }
+        }
+        
+        @ObservationIgnored
+        var fetchRequest: NSFetchRequest<ResultType>! {
+            didSet {
+                _fetchedResultsController = nil
+            }
+        }
+        
+        @ObservationIgnored
+        var _fetchedResultsController: NSFetchedResultsController<ResultType>? {
             didSet {
                 oldValue?.delegate = nil
-                fetchedResultsController?.delegate = self
+                _fetchedResultsController?.delegate = self
                 _result = nil
+            }
+        }
+        var fetchedResultsController: NSFetchedResultsController<ResultType>! {
+            get {
+                if _fetchedResultsController == nil {
+                    _fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+                }
+                return _fetchedResultsController
+            }
+            set {
+                _fetchedResultsController = newValue
             }
         }
         
         func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-            result = Result.success(controller.fetchedObjects as? [ResultType] ?? [])
+            _result = .success(controller.fetchedObjects as! [ResultType])
         }
     
-        private var _result: Result<[ResultType], Error>?
+        var _result: Result<[ResultType], Error>?
         var result: Result<[ResultType], Error>! {
             get {
                 if _result == nil {
                     do {
-                        try fetchedResultsController?.performFetch()
-                        _result = Result.success(fetchedResultsController?.fetchedObjects ?? [])
+                        let frc = fetchedResultsController!
+                        try frc.performFetch()
+                        _result = Result.success(frc.fetchedObjects!)
                     }
                     catch {
                         _result = Result.failure(error)
                     }
                 }
                 return _result!
-            }
-            set {
-                objectWillChange.send()
-                _result = newValue
             }
         }
     }
